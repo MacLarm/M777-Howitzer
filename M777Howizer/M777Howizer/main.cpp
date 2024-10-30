2da732e56eeb1126edcd067a20cb43388667a5d5
McKay Larman <lmlarman@byui.edu>
2024-10-30 13:08:28 -0600

    built the project with a gitignore.

---
+++ M777Howizer/M777Howizer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  M777Howizer
+//
+//  Created by Mckay Larman on 10/30/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+   // insert code here...
+   std::cout << "Hello, World!\n";
+   return 0;
+}
