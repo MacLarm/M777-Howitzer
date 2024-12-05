f016e87dd4a290347c366ac8c11e72f81d6431fd
McKay Larman <lmlarman@byui.edu>
2024-12-04 22:27:32 -0700

    added all the new files.

--- M777Howizer/M777Howizer/main.cpp
+++ M777Howizer/M777Howizer/main.cpp
@@ -1,26 +1,72 @@
-/***********************************************************************
- * Source File:
- *    MAIN
- * Author:
- *    Br. Helfrich
- * Summary:
- *    Launches the unit tests - that is all!
- ************************************************************************/
+/**********************************************************************
+* Source File:
+*    Lab 12: M777 Howitzer
+* Author:
+*    <your name here>
+* Summary:
+*    Simulate firing the M777 howitzer 15mm artillery piece
+************************************************************************/
 
-#include "test.h"
-#include "position.h"
+#include <cassert>      // for ASSERT
+#include "uiInteract.h" // for INTERFACE
+#include "uiDraw.h"     // for RANDOM and DRAW*
+#include "simulation.h" // for SIMULATION
+#include "position.h"   // for POSITION
+#include "test.h"       // for the unit tests
+using namespace std;
+
+
+/*************************************
+* All the interesting work happens here, when
+* I get called back from OpenGL to draw a frame.
+* When I am finished drawing, then the graphics
+* engine will wait until the proper amount of
+* time has passed and put the drawing on the screen.
+**************************************/
+void callBack(const Interface* pUI, void* p)
+{
+  // the first step is to cast the void pointer into a simulator object. This
+  // is the first step of every single callback function in OpenGL.
+  Simulator* pSim = (Simulator*)p;
+
+  ogstream gout;
+  Position pos(10,10);
+  gout = pos;
+  gout << "Hello world";
+}
 
 double Position::metersFromPixels = 40.0;
 
- /************************************
-  * MAIN
-  * There can be only one!
-  ************************************/
-int main()
+/*********************************
+* Initialize the simulation and set it in motion
+*********************************/
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI wWinMain(
+  _In_ HINSTANCE hInstance,
+  _In_opt_ HINSTANCE hPrevInstance,
+  _In_ PWSTR pCmdLine,
+  _In_ int nCmdShow)
+#else // !_WIN32
+int main(int argc, char** argv)
+#endif // !_WIN32
 {
-   // run the test code.
-   testRunner();
+  // unit tests
+  testRunner();
+ 
+  // Initialize OpenGL
+  Position posUpperRight;
+  posUpperRight.setZoom(40.0 /* 40 meters equals 1 pixel */);
+  posUpperRight.setPixelsX(700.0);
+  posUpperRight.setPixelsY(500.0);
+  Interface ui("M777 Howitzer Simulation", posUpperRight);
 
-   // return success
-   return 0;
+  // Initialize the simulation.
+  Simulator sim(posUpperRight);
+
+
+  // set everything into action
+  ui.run(callBack, (void *)&sim);
+
+  return 0;
 }
